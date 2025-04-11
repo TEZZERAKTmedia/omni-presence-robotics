853991f05018d8850014fda74d0824c6a0a24150
Trentyn Nicholas <trentynnicholas@Trentyns-MacBook-Pro.local>
2025-04-10 19:14:42 -0600

    Added cat toy servo logic

---
+++ Backend/Server/cat_toy_servo.py
@@ -0,0 +1,17 @@
+from servo import Servo
+
+
+
+servo_controller = Servo()
+
+def control_cat_toy(direction, str):
+    channel = '2'
+    if direction == 'left':
+        print("[CAT TOY] Spinning left")
+        servo_controller.pmw_servo.set_servo_pulse(servo_controller.pmw_channel_map[channel], 1300)
+    elif direction == 'right':
+        print("[CAT TOY] Spinning right")
+        servo_controller.pmw_servo.set_servo_pulse(servo_controller.pmw_schannel_map[channel], 1700)
+    else:
+        print("[CAT TOY] Stopping")
+        servo_controller.pmw_servo.set_servo_pulse(servo_controller.pmw_channel_map[channel], 1500)
