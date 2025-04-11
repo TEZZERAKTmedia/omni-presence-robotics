16b73d6d5881d3f8f4baa728c1f2edcae102223b
Trentyn Nicholas <trentynnicholas@Trentyns-MacBook-Pro.local>
2025-04-10 19:36:19 -0600

    Testing motor speed

--- Backend/Server/cat_toy_servo.py
+++ Backend/Server/cat_toy_servo.py
@@ -8,10 +8,10 @@ def control_cat_toy(direction, str=None):
     channel = '2'
     if direction == 'left':
         print("[CAT TOY] Spinning left")
-        servo_controller.pmw_servo.set_servo_pulse(servo_controller.pmw_channel_map[channel], 1300)
+        servo_controller.pmw_servo.set_servo_pulse(servo_controller.pmw_channel_map[channel], 1000)  # Faster left
     elif direction == 'right':
         print("[CAT TOY] Spinning right")
-        servo_controller.pmw_servo.set_servo_pulse(servo_controller.pmw_channel_map[channel], 1700)
+        servo_controller.pmw_servo.set_servo_pulse(servo_controller.pmw_channel_map[channel], 2000)  # Faster right
     else:
         print("[CAT TOY] Stopping")
-        servo_controller.pmw_servo.set_servo_pulse(servo_controller.pmw_channel_map[channel], 1500)
+        servo_controller.pmw_servo.set_servo_pulse(servo_controller.pmw_channel_map[channel], 1500)  # Stop
